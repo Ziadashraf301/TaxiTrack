71cc7b25d23cc7939129a6c111416067dcb990f7
Ziadashraf301 <ziadashraf98765@gmail.com>
2025-07-26 20:32:36 +0300

    TaxiTrack v6

--- dbt/target/run/my_dbt_project/models/marts/marts_green_tripdata_summary_all.sql
+++ dbt/target/run/my_dbt_project/models/marts/marts_green_tripdata_summary_all.sql
@@ -5,7 +5,7 @@
             where (
                 trip_uid) in (
                 select (trip_uid)
-                from "green_tripdata_summary_all__dbt_tmp164135846849"
+                from "green_tripdata_summary_all__dbt_tmp164603700239"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "ingest_db"."public"."green_tripdata_summary_all" ("trip_uid", "vendor_label", "trip_month", "trip_quarter", "trip_day", "trip_weekday", "day_type", "trip_hour", "payment_type_label", "trip_type", "trip_count", "avg_distance", "total_distance", "avg_fare", "total_fare", "total_tip", "avg_tip", "avg_total", "avg_passenger_count", "unique_pickup_locations", "first_trip", "last_trip", "peak_morning_hours", "peak_evening_hours", "trip_duration_minutes", "tip_rate")
     (
         select "trip_uid", "vendor_label", "trip_month", "trip_quarter", "trip_day", "trip_weekday", "day_type", "trip_hour", "payment_type_label", "trip_type", "trip_count", "avg_distance", "total_distance", "avg_fare", "total_fare", "total_tip", "avg_tip", "avg_total", "avg_passenger_count", "unique_pickup_locations", "first_trip", "last_trip", "peak_morning_hours", "peak_evening_hours", "trip_duration_minutes", "tip_rate"
-        from "green_tripdata_summary_all__dbt_tmp164135846849"
+        from "green_tripdata_summary_all__dbt_tmp164603700239"
     )
   
